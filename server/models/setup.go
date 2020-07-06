b2984a4e5e1ea355b33b67a9b67244ca3e524bc0
MemoVDG <memovdg@gmail.com>
2020-07-06 15:34:17 -0500

    [ADD] Postgresql migration

--- server/models/setup.go
+++ server/models/setup.go
@@ -1,17 +1,26 @@
 package models
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+// DB : DB object
 var DB *gorm.DB
 
 // ConnectDataBase : Starting the DB
-func ConnectDataBase(dbPath string) {
-	database, err := gorm.Open("sqlite3", dbPath)
+func ConnectDataBase() {
+
+	dbData := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", os.Getenv("HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PSW"))
+	fmt.Println(dbData)
+	database, err := gorm.Open("postgres", dbData)
 
 	if err != nil {
+		log.Println(err)
 		panic("Failed to connect to database!")
 	}
 
